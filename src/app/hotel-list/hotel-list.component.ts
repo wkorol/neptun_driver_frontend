06c35ce69cd990273fd66021a363e3fee2f9e7cc
Wiktor Korol <wiktor.korol@iteo.com>
2024-11-11 02:06:15 +0100

    code fixes

--- src/app/hotel-list/hotel-list.component.ts
+++ src/app/hotel-list/hotel-list.component.ts
@@ -3,6 +3,9 @@ import { Router } from '@angular/router';
 import {Hotel, HotelService} from '../hotel.service';
 import {MatCard, MatCardContent, MatCardHeader, MatCardSubtitle, MatCardTitle} from "@angular/material/card";
 import {NgForOf} from "@angular/common";
+import {MatFormField, MatLabel} from "@angular/material/form-field";
+import {MatInput} from "@angular/material/input";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-hotel-list',
@@ -14,12 +17,17 @@ import {NgForOf} from "@angular/common";
     MatCardHeader,
     MatCardTitle,
     MatCardContent,
-    NgForOf
+    MatLabel,
+    NgForOf,
+    MatFormField,
+    MatInput,
+    FormsModule
   ],
   styleUrls: ['./hotel-list.component.css']
 })
 export class HotelListComponent implements OnInit {
   hotels: Hotel[] = [];
+  searchTerm: string = '';
 
   constructor(private hotelService: HotelService, private router: Router) {}
 
@@ -40,5 +48,14 @@ export class HotelListComponent implements OnInit {
 
   viewHotelDetails(hotelId: string): void {
     this.router.navigate([`hotel/${hotelId}`]);
+  }
+
+  filteredHotels(): Hotel[] {
+    if (!this.searchTerm) {
+      return this.hotels;
+    }
+    return this.hotels.filter(hotel =>
+        hotel.name.toLowerCase().includes(this.searchTerm.toLowerCase())
+    );
   }
 }
