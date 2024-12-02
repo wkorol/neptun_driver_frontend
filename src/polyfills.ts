ba52f5cc73198fd07be6fd027bb92c4966778e3d
Wiktor Korol <wiktor.korol@iteo.com>
2024-12-02 23:14:24 +0100

    Code fixes

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,10 +1,2 @@
-/***************************************************************************************************
- * Angular Required Polyfills
- */
-import 'zone.js'; // Angular's required polyfill for change detection.
-
-/***************************************************************************************************
- * Polyfills for ES6+ Features
- */
-import 'core-js/stable'; // Includes ES6+ features like Promises, Map, etc.
-import 'core-js/es'; // Additional ES6+ support for older browsers.
+import 'core-js/es'; // ES6+ polyfills
+import 'zone.js'; // Angular's Zone.js (required for change detection)
