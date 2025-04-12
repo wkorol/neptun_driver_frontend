5f31a7f68a6f373580708d03406b58218d37e3e9
Wiktor Korol <wiktor.korol@iteo.com>
2025-04-12 18:18:58 +0200

    test

--- src/app/driver-status/driver-status.component.ts
+++ src/app/driver-status/driver-status.component.ts
@@ -1,14 +1,15 @@
-import { Component } from '@angular/core';
-import {expand, interval, of, Subscription, switchMap} from "rxjs";
-import {HttpClient} from "@angular/common/http";
-import {GoogleMap, MapMarker} from "@angular/google-maps";
-import {NgForOf} from "@angular/common";
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { expand, of, delay, switchMap, Subscription } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { GoogleMap, MapMarker } from '@angular/google-maps';
+import { NgForOf } from '@angular/common';
 
 interface TaxiStatus {
   TaxiNo: string;
   Latitude: number;
   Longitude: number;
 }
+
 @Component({
   selector: 'app-driver-status',
   standalone: true,
@@ -20,16 +21,14 @@ interface TaxiStatus {
   templateUrl: './driver-status.component.html',
   styleUrl: './driver-status.component.css'
 })
-export class DriverStatusComponent {
+export class DriverStatusComponent implements OnInit, OnDestroy {
   center: google.maps.LatLngLiteral = { lat: 54.352025, lng: 18.646638 }; // Gdańsk
   zoom = 12;
 
   taxis: TaxiStatus[] = [];
   private subscription?: Subscription;
 
-  constructor(private http: HttpClient) {
-
-  }
+  constructor(private http: HttpClient) {}
 
   ngOnInit() {
     this.subscription = of(null).pipe(
@@ -37,7 +36,7 @@ export class DriverStatusComponent {
             this.http.get<TaxiStatus[]>('https://apineptun-ij5mx.ondigitalocean.app/api/proxy/drivers/status').pipe(
                 switchMap(data => {
                   this.taxis = data;
-                  return of(null);
+                  return of(null).pipe(delay(500)); // krótka przerwa przed następnym zapytaniem
                 })
             )
         )
